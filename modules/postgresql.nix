3ce9cc6f00178c3cce801746467405eaab01df1b
Stefan Lau <github@stefanlau.com>
2020-11-08 15:47:31 +0100

    Fix postgres startup

--- modules/postgresql.nix
+++ modules/postgresql.nix
@@ -1,19 +1,5 @@
 { config, lib, pkgs, ... }: let
     secrets = import ./lib/secrets.nix;
-    bootstrapPostgres = pkgs: pkgs.writeScriptBin "bootstrap-postgresql" ''
-        RETRIES=5
-
-        until ${pkgs.sudo}/bin/sudo -u postgres ${pkgs.postgresql_10}/bin/psql -d postgres -c "" 2> /dev/null || [ $RETRIES -eq 0 ]; do
-            echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
-            sleep 5
-        done
-
-        ${pkgs.sudo}/bin/sudo -u postgres ${pkgs.postgresql_10}/bin/createuser nextcloud || echo "Error, probably user already exists";
-        ${pkgs.sudo}/bin/sudo -u postgres ${pkgs.postgresql_10}/bin/createdb nextcloud || echo "Error, probably db already exists";
-
-        ${pkgs.sudo}/bin/sudo -u postgres ${pkgs.postgresql_10}/bin/psql -c "ALTER USER nextcloud WITH PASSWORD '${secrets.getBash "postgresql/nextcloud"}'";
-        ${pkgs.sudo}/bin/sudo -u postgres ${pkgs.postgresql_10}/bin/psql -c "ALTER DATABASE nextcloud OWNER TO nextcloud";
-    '';
 in {
     containers.postgresql = {
         autoStart = true;
@@ -26,15 +12,9 @@ in {
         }  // (secrets.mountSecrets lib [
             "postgresql"
         ]);
-        config = { pkgs, lib, ... }: let
-                bootstapScript = bootstrapPostgres pkgs;
-            in {
+        config = { pkgs, lib, ... }: {
             time.timeZone = config.homeserver.timeZone;
             system.stateVersion = config.system.stateVersion;
-
-            environment.systemPackages = [
-                bootstapScript
-            ];
 
             services.postgresql = {
                 enable = true;
@@ -49,11 +29,16 @@ in {
                     host    all             all             127.0.0.1/32            password
                     host    all             all             ::1/128                 password
                 '';
+                ensureDatabases = [ "nextcloud" ];
+                ensureUsers = [
+                    {
+                        name = "nextcloud";
+                        ensurePermissions = {
+                            "DATABASE nextcloud" = "ALL PRIVILEGES";
+                        };
+                    }
+                ];
             };
-
-            systemd.services.postgresql.postStart = ''
-                ${bootstapScript}/bin/bootstrap-postgresql
-            '';
         };
     };
 
